03530bafa3e1613db17c2547ef46bb85b758e03b
nangelos <nicksangelos@gmail.com>
2019-02-19 14:24:09 -0600

    [navbar] setting up routes based on access privileges

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,14 +4,17 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+const Navbar = ({handleClick, isLoggedIn, isAdmin, isHeadCoach}) => (
   <div>
-    <h1>Blue Chip Hoops</h1>
     <nav className="navbar navbar-dark bg-primary">
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
+          <Link to="/recruits">Recruits</Link>
+          <Link to="/calendar">Calendar</Link>
+          {isHeadCoach ? <Link to="Staff">Staff</Link> : <span />}
+          {isAdmin ? <Link to="/admin">Admin</Link> : <span />}
           <a href="#" onClick={handleClick}>
             Logout
           </a>
@@ -33,7 +36,9 @@ const Navbar = ({handleClick, isLoggedIn}) => (
  */
 const mapState = state => {
   return {
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.id,
+    isAdmin: state.user.isAdmin,
+    isHeadCoach: state.user.role === 'Head Coach'
   }
 }
 
