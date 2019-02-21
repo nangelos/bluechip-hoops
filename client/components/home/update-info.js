3da432bc198425f1bee34136584f601868fcf674
nangelos <nicksangelos@gmail.com>
2019-02-21 11:09:20 -0600

    [home/update-info] conditional rendering based on userInfo

--- client/components/home/update-info.js
+++ client/components/home/update-info.js
@@ -13,8 +13,14 @@ class UserData extends Component {
 
   handleSubmit = evt => {
     evt.preventDefault()
+    const {userInfo} = this.props.state
     const {createUserInfo} = this.props
-    createUserInfo(this.state)
+    if (!userInfo['user-info']) {
+      createUserInfo(this.state)
+    } else {
+      const {firstName, lastName, school, role} = userInfo['user-info']
+      console.log('udpate info')
+    }
   }
 
   handleTextChange = evt => {
@@ -24,7 +30,7 @@ class UserData extends Component {
 
   render() {
     let info = this.props.state.userInfo['user-info']
-    return (
+    return info ? (
       <div style={{textAlign: 'center'}}>
         <h1>Update User Info</h1>
         <div>
@@ -35,7 +41,64 @@ class UserData extends Component {
                 <input
                   type="text"
                   name="firstName"
-                  // placeholder={info.firstName}
+                  placeholder={info.firstName}
+                  className="user-info-input"
+                  onChange={this.handleTextChange}
+                />
+              </div>
+              <div className="user-input-row">
+                Last Name <br />
+                <input
+                  type="text"
+                  name="lastName"
+                  placeholder={info.lastName}
+                  className="user-info-input"
+                  onChange={this.handleTextChange}
+                />
+              </div>
+              <div className="user-input-row">
+                School <br />
+                <select
+                  name="school"
+                  onChange={this.handleTextChange}
+                  selected={info.school}
+                >
+                  {schoolList.map((school, i) => {
+                    return (
+                      <option key={i} value={school}>
+                        {school}
+                      </option>
+                    )
+                  })}
+                </select>
+              </div>
+              <div className="user-input-row">
+                Role <br />
+                <select
+                  name="role"
+                  onChange={this.handleTextChange}
+                  selected={info.role}
+                >
+                  <option value="Head Coach">Head Coach</option>
+                  <option value="Assistant Coach">Assistant Coach</option>
+                </select>
+              </div>
+            </div>
+            <input type="submit" value="Update" />
+          </form>
+        </div>
+      </div>
+    ) : (
+      <div style={{textAlign: 'center'}}>
+        <h1>Update User Info</h1>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <div className="user-input-row">
+                First Name <br />
+                <input
+                  type="text"
+                  name="firstName"
                   className="user-info-input"
                   onChange={this.handleTextChange}
                 />
