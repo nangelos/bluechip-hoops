aa8a93a0a4d1653500947c175ac18b476f9aa0dc
nangelos <nicksangelos@gmail.com>
2019-02-27 10:32:40 -0600

    [styles & recruits] mapping through array of recruits and styling them

--- client/components/recruits/index.js
+++ client/components/recruits/index.js
@@ -6,15 +6,17 @@ const recruitList = [
     highSchool: 'Laney High',
     position: 'SG',
     height: "6'5",
+    weight: '195',
     rivalsRanking: '1',
     espnRanking: '1',
     class: '2019'
   },
   {
     fullName: 'LeBron James',
-    highSchool: 'SVSM',
+    highSchool: 'St. Vincent St. Mary',
     position: 'SF',
     height: "6'8",
+    weight: '230',
     rivalsRanking: '1',
     espnRanking: '1',
     class: '2019'
@@ -26,17 +28,74 @@ class Recruits extends Component {
 
   render() {
     return (
-      <div>
+      <div align="center">
         <h1>This is the recruits page</h1>
-        <div>
-          {recruitList.map(player => {
-            return (
-              <div key={player.fullName}>
-                <span>{player.fullName}</span>
-              </div>
-            )
-          })}
-        </div>
+        <table style={{border: 'solid, white, 2px'}}>
+          <tbody>
+            <tr>
+              <th className="recruits-header">Name</th>
+              <th className="recruits-header">Position</th>
+              <th className="recruits-header">Height</th>
+              <th className="recruits-header">Weight</th>
+              <th className="recruits-header">High School</th>
+              <th className="recruits-header">Rivals Ranking</th>
+              <th className="recruits-header">ESPN Ranking</th>
+              <th className="recruits-header">Class</th>
+            </tr>
+
+            {recruitList.map((player, i) => {
+              return (
+                <tr key={player.fullName}>
+                  <td className={i % 2 === 0 ? 'odd-row' : 'even-row'}>
+                    {player.fullName}
+                  </td>
+                  <td
+                    className={i % 2 === 0 ? 'odd-row' : 'even-row'}
+                    style={{textAlign: 'center'}}
+                  >
+                    {player.position}
+                  </td>
+                  <td
+                    className={i % 2 === 0 ? 'odd-row' : 'even-row'}
+                    style={{textAlign: 'center'}}
+                  >
+                    {player.height}
+                  </td>
+                  <td
+                    className={i % 2 === 0 ? 'odd-row' : 'even-row'}
+                    style={{textAlign: 'center'}}
+                  >
+                    {player.weight}
+                  </td>
+                  <td
+                    className={i % 2 === 0 ? 'odd-row' : 'even-row'}
+                    style={{textAlign: 'center'}}
+                  >
+                    {player.highSchool}
+                  </td>
+                  <td
+                    className={i % 2 === 0 ? 'odd-row' : 'even-row'}
+                    style={{textAlign: 'center'}}
+                  >
+                    {player.rivalsRanking}
+                  </td>
+                  <td
+                    className={i % 2 === 0 ? 'odd-row' : 'even-row'}
+                    style={{textAlign: 'center'}}
+                  >
+                    {player.espnRanking}
+                  </td>
+                  <td
+                    className={i % 2 === 0 ? 'odd-row' : 'even-row'}
+                    style={{textAlign: 'center'}}
+                  >
+                    {player.class}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </div>
     )
   }
