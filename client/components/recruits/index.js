4c8b4380649de78c5c7f8a0285934032931be6dd
nangelos <nicksangelos@gmail.com>
2019-02-26 19:33:59 -0600

    [recruits] adding player row to map through an array of objects

--- client/components/recruits/index.js
+++ client/components/recruits/index.js
@@ -1,5 +1,26 @@
 import React, {Component} from 'react'
 
+const recruitList = [
+  {
+    fullName: 'Michael Jordan',
+    highSchool: 'Laney High',
+    position: 'SG',
+    height: "6'5",
+    rivalsRanking: '1',
+    espnRanking: '1',
+    class: '2019'
+  },
+  {
+    fullName: 'LeBron James',
+    highSchool: 'SVSM',
+    position: 'SF',
+    height: "6'8",
+    rivalsRanking: '1',
+    espnRanking: '1',
+    class: '2019'
+  }
+]
+
 class Recruits extends Component {
   state = {}
 
@@ -7,6 +28,15 @@ class Recruits extends Component {
     return (
       <div>
         <h1>This is the recruits page</h1>
+        <div>
+          {recruitList.map(player => {
+            return (
+              <div key={player.fullName}>
+                <span>{player.fullName}</span>
+              </div>
+            )
+          })}
+        </div>
       </div>
     )
   }
