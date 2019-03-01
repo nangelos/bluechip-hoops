4834845dcb6f9d5dab7a3bc55a4d1aeed683417b
nangelos <nicksangelos@gmail.com>
2019-03-01 14:57:49 -0600

    [recruits styling] using styled-components for condl rendering... still not working

--- client/components/recruits/tabs.js
+++ client/components/recruits/tabs.js
@@ -1,27 +1,43 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const Tab = styled.button`
+  font-weight: ${props => (props.selectedYear ? 'bold' : 'normal')};
+  background-color: #fff;
+  border-color: #dee2e6 #dee2e6 #fff;
+  border: 2px solid transparent;
+  border-top-left-radius: 0.5rem;
+  border-top-right-radius: 0.5rem;
+  padding: 5px;
+  width: 70px;
+`
+
+const AllTabs = styled.ul`
+  display: flex;
+  margin-bottom: 0px;
+  height: 40px;
+`
 const TabRow = props => {
   let {changeYear, years, selectedYear} = props
   console.log('tab.js props: ', props)
   return (
     <div>
-      <ul className="tab-row">
+      <AllTabs>
         {years.map(year => {
           return (
-            <li key={year}>
-              <button
-                type="submit"
-                onClick={changeYear}
-                style={{border: 'none'}}
-                value={year}
-                className={year === selectedYear ? 'active' : null}
-              >
-                {year}
-              </button>
-            </li>
+            <Tab
+              key={year}
+              type="submit"
+              onClick={changeYear}
+              style={{border: 'none'}}
+              value={year}
+              // className={year === selectedYear ? 'active' : null}
+            >
+              {year}
+            </Tab>
           )
         })}
-      </ul>
+      </AllTabs>
     </div>
   )
 }
